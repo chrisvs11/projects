ef893fb12bda926ee121aeef52ba1748775a66f9
Christian Vargas <chrisvs11@hotmail.com>
2024-11-14 21:36:00 -0600

    feat(main):correct bug

--- pacmen/frontend/src/shared/providers/providers.tsx
+++ pacmen/frontend/src/shared/providers/providers.tsx
@@ -1,28 +1,22 @@
 "use client";
 
-import { CacheProvider } from "@chakra-ui/next-js";
-
 import { ChakraProvider } from "@chakra-ui/react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { RecoilRoot } from "recoil";
+
 import { AudioBtn } from "../components";
 
 const queryClient = new QueryClient();
 
-
-
 export function Providers({ children }: { children: React.ReactNode }) {
-
   return (
     <RecoilRoot>
-      <AudioBtn className={"audio_btn_container"}/>
-      <CacheProvider>
-        <QueryClientProvider client={queryClient}>
-          <ChakraProvider>{children}</ChakraProvider>
-        </QueryClientProvider>
-      </CacheProvider>
+      <AudioBtn className={"audio_btn_container"} />
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>{children}</ChakraProvider>
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
